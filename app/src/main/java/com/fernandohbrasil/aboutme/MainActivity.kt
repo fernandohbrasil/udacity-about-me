44faeb640aa2b18c80fd5d64775cfa2215c0dc58
fernandohbrasil <fernandohbrasil@hotmail.com>
2020-03-01 21:08:32 +0000

    first commit

---
+++ app/src/main/java/com/fernandohbrasil/aboutme/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.fernandohbrasil.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.fernandohbrasil.aboutme.databinding.ActivityMainBinding
+import com.fernandohbrasil.aboutme.model.Name
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val name: Name = Name(getString(R.string.name))
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.name = name
+
+        setContentView(binding.root)
+
+        bind()
+    }
+
+    private fun bind() {
+        binding.doneButton.setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+        binding.apply {
+            name?.nickName = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = GONE
+            doneButton.visibility = GONE
+            nicknameText.visibility = VISIBLE
+        }
+
+        hideKeyBoard(view)
+    }
+
+    private fun hideKeyBoard(view: View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
---
+++ app/src/main/java/com/fernandohbrasil/aboutme/model/Name.kt
@@ -0,0 +1,6 @@
+package com.fernandohbrasil.aboutme.model
+
+class Name(
+    var name: String = "",
+    var nickName: String = ""
+)
